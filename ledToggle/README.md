afbdb59cca4f0dddfc9dd38302287adb9cf04c3b
Simon Kölsch <echox@echox.de>
2013-08-25 21:08:33 +0200

    init

---
+++ ledToggle/README.md
@@ -0,0 +1,18 @@
+ledToggle
+=========
+
+Scope
+-----
+Turn a LED on/off via arbitrary input.
+
+Toggle is implemented with a reed switch.
+If LED is turned off, IC is in power safe.
+
+Circuit
+-------
+//TODO
+
+BOM
+---
+ATTiny 45
+//TODO
---
+++ ledToggle/ledToggle.c
@@ -0,0 +1,86 @@
+#include <avr/io.h> 
+#include <avr/interrupt.h> 
+#include <util/delay.h> 
+#include <avr/sleep.h>
+
+#define BUTTON1 (1<<PB1) 
+#define LED     (1<<PB2) 
+
+
+     ISR(SIG_PIN_CHANGE)
+   { 
+	cli();
+//	sleep_disable();
+		int i =0;
+		for(i=0;i<5;i++) {
+				_delay_ms(500);
+                        	PORTB ^= LED; 
+			}
+   } 
+
+
+int main() 
+{ 
+
+        /* outputs */ 
+        DDRB |= LED; 
+
+        /* internal pullups */ 
+        PORTB |= BUTTON1;
+
+	PCMSK |= (1<<PCINT1);
+//	GIFR |= (1<<PCIF);
+	GIMSK |= (1<<PCIE);
+
+cli();
+
+	PORTB |= LED;
+
+//sei();
+
+        for (;;) { 
+cli();
+
+	
+  //                      PORTB ^= LED; 
+//			_delay_ms(5500);
+
+                if (!(PINB & BUTTON1)) { 
+                        /* toggle led */ 
+//                        PORTB ^= LED; 
+
+                        /* prinmitive debounce */ 
+                       _delay_ms(1000); 
+
+			while(!(PINB & BUTTON1)) {
+				_delay_ms(100);
+                        	PORTB ^= LED; 
+			}
+
+			sei();
+			sleep_enable();
+			set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+			sleep_mode();
+			
+			
+				_delay_ms(100);
+                        	PORTB ^= LED; 
+
+				_delay_ms(100);
+                        	PORTB ^= LED; 
+
+				_delay_ms(1000);
+                        	PORTB ^= LED; 
+
+				_delay_ms(100);
+                        	PORTB ^= LED; 
+
+				_delay_ms(100);
+                        	PORTB ^= LED; 
+		}			
+	} 
+        
+
+        /* NOTREACHED */ 
+        return (0); 
+}
